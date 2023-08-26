77ab6ba46db7d1c8c380f778f28e92d4dff4c333
Pavlo Lysiuk <pavel.lysiuk@gmail.com>
2023-08-26 15:13:46 +0300

    task done

---
+++ src/components/Section/Section.styled.js
@@ -0,0 +1,13 @@
+import { styled } from 'styled-components';
+
+export const SectionWrapper = styled.div`
+`;
+
+export const SectionTitle = styled.div`
+  margin-bottom: 16px;
+
+  font-size: 24px;
+  font-weight: 700;
+  text-align: center;
+  text-transform: uppercase;
+`;
